9cd3b917e480ed7750fb333007a07d95f5a20c7a
Brian Jackson <brianjackson1224@gmail.com>
2021-05-19 12:01:52 -0700

    Initial commit

---
+++ src/cs350s21project/cli/CommandInterpreter.java
@@ -0,0 +1,10 @@
+package cs350s21project.cli;
+
+import cs350s21project.controller.CommandManagers;
+
+public class CommandInterpreter {
+
+	public void evaluate(String command) {
+		System.out.println("Hello World!");
+	}
+}
