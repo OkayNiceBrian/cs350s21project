30a1525e8ad36e44d7ec341d18da7232b3bbd228
MarkCapka <markcapka@gmail.com>
2021-05-20 16:16:05 -0700

    Initial Commit

--- cli/CommandInterpreter.java
+++ cli/CommandInterpreter.java
@@ -1,9 +1,11 @@
 package cs350s21project.cli;
 
-import cs350s21project.controller.CommandManagers;
 
 public class CommandInterpreter {
 
+	
+	
+	
 	public void evaluate(String command) {
 		System.out.println("Hello World!");
 	}
