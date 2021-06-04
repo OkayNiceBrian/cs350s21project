7aaf5a7077949e022f9193057f75dc3991bcc893
Tom Rohr <60917364+Tom-Rohr@users.noreply.github.com>
2021-06-04 12:51:11 -0700

    Add files via upload

--- src/cs350s21project/cli/MiscCmdFactory.java
+++ src/cs350s21project/cli/MiscCmdFactory.java
@@ -41,38 +41,87 @@ public class MiscCmdFactory {
 				return forceCmd(managers, cliIn);
 			case "create":
 				AgentID createID = new AgentID(command[2]);
-				int size = Integer.valueOf(command[5]);
+				int size = Integer.valueOf(command[6]);
 				
-				String[] lat1Str = command[6].split("'/*");//lat1
-					int lat1hr = Integer.valueOf(lat1Str[0]); 
-					int lat1min = Integer.valueOf(lat1Str[1]);
-					int lat1sec = Integer.valueOf(lat1Str[2]);
-				String[] lat2Str = command[7].split("'/*");//lat2
-					int lat2hr = Integer.valueOf(lat2Str[0]); 
-					int lat2min = Integer.valueOf(lat2Str[1]);
-					int lat2sec = Integer.valueOf(lat2Str[2]);
-				String[] lat3Str = command[8].split("'/*");//lat3
-					int lat3hr = Integer.valueOf(lat3Str[0]); 
-					int lat3min = Integer.valueOf(lat3Str[1]);
-					int lat3sec = Integer.valueOf(lat3Str[2]);
-				String[] lon1Str = command[9].split("'/*");//lon1
-					int lon1hr = Integer.valueOf(lon1Str[0]); 
-					int lon1min = Integer.valueOf(lon1Str[1]);
-					int lon1sec = Integer.valueOf(lon1Str[2]);
-				String[] lon2Str = command[10].split("'/*");//lon2
-					int lon2hr = Integer.valueOf(lon2Str[0]); 
-					int lon2min = Integer.valueOf(lon2Str[1]);
-					int lon2sec = Integer.valueOf(lon2Str[2]);
-				String[] lon3Str = command[11].split("'/*");//lon3
-					int lon3hr = Integer.valueOf(lon3Str[0]); 
-					int lon3min = Integer.valueOf(lon3Str[1]);
-					int lon3sec = Integer.valueOf(lon3Str[2]);
-				Latitude lat1 = new Latitude(lat1hr, lat1min ,lat1sec);//command[6]
-				Latitude lat2 = new Latitude(lat2hr, lat2min ,lat2sec);//command[7]
-				Latitude lat3 = new Latitude(lat3hr, lat3min ,lat3sec);//command[8]
-				Longitude lon1 = new Longitude(lon1hr, lon1min ,lon1sec);//command[9]
-				Longitude lon2 = new Longitude(lon2hr, lon2min ,lon2sec);//command[10]
-				Longitude lon3 = new Longitude(lon3hr, lon3min ,lon3sec);//command[11]
+				//command[] 7,8,9,10,11,12 are not broken into individual strings each containing a lat or lon.
+				
+				int lat1hr =0, lat1min =0;
+				int lat2hr =0, lat2min =0;
+				int lat3hr =0, lat3min =0;
+				int lon1hr =0, lon1min =0;
+				int lon2hr=0, lon2min =0;
+				int lon3hr=0, lon3min=0;
+				double lat1sec=0, lat2sec=0, lat3sec=0, lon1sec=0, lon2sec=0, lon3sec=0;
+				
+				/////////////////LATITUDE 1//////////////////////////
+				String[] lat1StrHr = command[7].split("\\*");
+				String[] lat1StrMin = lat1StrHr[1].split("\'");
+				String[] lat1StrSec = lat1StrMin[1].split("\"");
+				
+				lat1hr = Integer.parseInt(lat1StrHr[0]);
+				lat1min = Integer.parseInt(lat1StrMin[0]);
+				lat1sec = Integer.parseInt(lat1StrSec[0]);
+				////////////////////////////////////////////////////
+				/////////////////LATITUDE 2/////////////////////////
+				String[] lat2StrHr = command[8].split("\\*");
+				String[] lat2StrMin = lat2StrHr[1].split("\'");
+				String[] lat2StrSec = lat2StrMin[1].split("\"");
+				
+				lat2hr = Integer.parseInt(lat2StrHr[0]);
+				lat2min = Integer.parseInt(lat2StrMin[0]);
+				lat2sec = Integer.parseInt(lat2StrSec[0]);
+				////////////////////////////////////////////////////
+				/////////////////LATITUDE 3/////////////////////////
+				String[] lat3StrHr = command[9].split("\\*");
+				String[] lat3StrMin = lat3StrHr[1].split("\'");
+				String[] lat3StrSec = lat3StrMin[1].split("\"");
+				
+				lat3hr = Integer.parseInt(lat3StrHr[0]);
+				lat3min = Integer.parseInt(lat3StrMin[0]);
+				lat3sec = Integer.parseInt(lat3StrSec[0]);
+				////////////////////////////////////////////////////
+				/////////////////LONGITUDE 1////////////////////////
+				String[] lon1StrHr = command[10].split("\\*");
+				String[] lon1StrMin = lon1StrHr[1].split("\'");
+				String[] lon1StrSec = lon1StrMin[1].split("\"");
+				
+				lon1hr = Integer.parseInt(lon1StrHr[0]);
+				lon1min = Integer.parseInt(lon1StrMin[0]);
+				lon1sec = Integer.parseInt(lon1StrSec[0]);
+				////////////////////////////////////////////////////
+				/////////////////LONGITUDE 2////////////////////////
+				String[] lon2StrHr = command[11].split("\\*");
+				String[] lon2StrMin = lon2StrHr[1].split("\'");
+				String[] lon2StrSec = lon2StrMin[1].split("\"");
+				
+				lon2hr = Integer.parseInt(lon2StrHr[0]);
+				lon2min = Integer.parseInt(lon2StrMin[0]);
+				lon2sec = Integer.parseInt(lon2StrSec[0]);
+				////////////////////////////////////////////////////
+				/////////////////LONGITUDE 3////////////////////////
+				String[] lon3StrHr = command[12].split("\\*");
+				String[] lon3StrMin = lat1StrHr[1].split("\'");
+				String[] lon3StrSec = lat1StrMin[1].split("\"");
+				
+				lon3hr = Integer.parseInt(lon3StrHr[0]);
+				lon3min = Integer.parseInt(lon3StrMin[0]);
+				lon3sec = Integer.parseInt(lon3StrSec[0]);
+				////////////////////////////////////////////////////
+				
+				System.out.println(lat1hr+" "+lat1min+" "+lat1sec);//TESTING
+				System.out.println(lat2hr+" "+lat2min+" "+lat2sec);//TESTING
+				System.out.println(lat3hr+" "+lat3min+" "+lat3sec);//TESTING
+				System.out.println(lon1hr+" "+lon1min+" "+lon1sec);//TESTING
+				System.out.println(lon2hr+" "+lon2min+" "+lon2sec);//TESTING
+				System.out.println(lon3hr+" "+lon3min+" "+lat3sec);//TESTING
+				
+				
+				Latitude lat1 = new Latitude(lat1hr, lat1min ,lat1sec);//command[7]
+				Latitude lat2 = new Latitude(lat2hr, lat2min ,lat2sec);//command[8]
+				Latitude lat3 = new Latitude(lat3hr, lat3min ,lat3sec);//command[9]
+				Longitude lon1 = new Longitude(lon1hr, lon1min ,lon1sec);//command[10]
+				Longitude lon2 = new Longitude(lon2hr, lon2min ,lon2sec);//command[11]
+				Longitude lon3 = new Longitude(lon3hr, lon3min ,lon3sec);//command[12]
 				
 				CommandViewCreateWindowTop topView = new CommandViewCreateWindowTop(managers, cliIn, createID, size, lat1, lat2, lat3, lon1, lon2, lon3);
 				return topView;
@@ -106,15 +155,43 @@ public class MiscCmdFactory {
 	public static CommandActorSetState forceCmd(CommandManagers managers, String cliIn) {
 		String[] command = cliIn.split(" ");
 		
-		AgentID id = new AgentID(command[1]);
+		//System.out.println(cliIn);//TEST
+
+		int lat1hr = 0, lat1min = 0; //Latitude variables
+		int lon1hr = 0, lon1min = 0;//Longitude variables
+		double lat1sec = 0, lon1sec = 0;//Lat & lon variables for seconds
+
+		String[] coords = command[4].split("/"); //Need data at index 0;
 		
-		String[] coords = command[4].split("*'/");
-			Latitude lat = new Latitude(Integer.valueOf(coords[0]),Integer.valueOf(coords[1]),Double.valueOf(coords[2]));
-			Longitude lon = new Longitude(Integer.valueOf(coords[3]),Integer.valueOf(coords[4]),Double.valueOf(coords[5]));
-			Altitude alt = new Altitude(Double.valueOf(coords[6]));
-			
-		CoordinateWorld3D position = new CoordinateWorld3D(lat, lon, alt);
-		Course course = new Course(Double.valueOf(command[8]));
+		for(int z=0; z<coords.length; z++) {
+			System.out.println(coords[z]);
+		}
+		String[] lat1StrHr = coords[0].split("\\*"); //Need data at index 0;
+		String[] lat1StrMin = lat1StrHr[1].split("\'"); //Need data at index 0;
+		String[] lat1StrSec = lat1StrMin[1].split("\"");//Need data at index 0;
+		
+		lat1hr = Integer.parseInt(lat1StrHr[0]);
+		lat1min = Integer.parseInt(lat1StrMin[0]);
+		lat1sec = Integer.parseInt(lat1StrSec[0]);
+		
+		String[] lon1StrHr = coords[1].split("\\*"); //Need data at index 0;
+		String[] lon1StrMin = lon1StrHr[1].split("\'"); //Need data at index 0;
+		String[] lon1StrSec = lon1StrMin[1].split("\"");//Need data at index 0;
+		
+		lon1hr = Integer.parseInt(lon1StrHr[0]);
+		lon1min = Integer.parseInt(lon1StrMin[0]);
+		lon1sec = Integer.parseInt(lon1StrSec[0]);
+		
+		System.out.println(lat1hr+" "+lat1min+" "+lat1sec);//TESTING
+		System.out.println(lon1hr+" "+lon1min+" "+lon1sec);//TESTING
+		
+		Latitude lat1 = new Latitude(lat1hr, lat1min ,lat1sec);
+		Longitude lon1 = new Longitude(lon1hr, lon1min, lon1sec);
+		Altitude alt1 = new Altitude(Double.valueOf(coords[2]));
+		CoordinateWorld3D position = new CoordinateWorld3D(lat1, lon1, alt1);
+		
+		AgentID id = new AgentID(command[1]);
+		Course course = new Course(Double.valueOf(command[7]));
 		Groundspeed speed = new Groundspeed(Double.valueOf(command[9]));
 		CommandActorSetState forceCmd = new CommandActorSetState(managers, cliIn, id, position, course, speed);
 		return forceCmd;
