8ebf93c3081f9def678f7f3689a1a64897d7bf5a
Brian Jackson <brianjackson1224@gmail.com>
2021-06-04 12:20:34 -0700

    Tester update

--- src/cs350s21project/cli/CommandInterpreter.java
+++ src/cs350s21project/cli/CommandInterpreter.java
@@ -14,16 +14,19 @@ public class CommandInterpreter {
 		String[] commands = command.split(";", 0);
 		
 		for (String commandString : commands) {
-			commandString = commandString.trim();
 			
 			// Handle comments
 			for (int i = 0; i < commandString.length(); i++) {
 				if (commandString.charAt(i) == '/' && i < commandString.length() - 1 && commandString.charAt(i + 1) == '/') {
-					commandString = commandString.substring(0, i) + commandString.substring(commandString.lastIndexOf("\n"), commandString.length() - 1);
+					commandString = commandString.substring(0, i) + commandString.substring(commandString.lastIndexOf("\n"), commandString.length());
 				}
 			}
 			
 			commandString = commandString.trim();
+			
+			if (commandString.equals("")) {
+				break;
+			}
 			
 			// Create an array of words in the command
 			String[] cmdArr = commandString.split(" ", 0);
@@ -69,7 +72,8 @@ public class CommandInterpreter {
 				throw new RuntimeException("Invalid command");
 			}
 
-			managers.schedule(builtCommand);
+			//managers.schedule(builtCommand);
+			System.out.println(builtCommand);
 		}
 	}
 }
--- src/cs350s21project/cli/tester.java
+++ src/cs350s21project/cli/tester.java
@@ -6,7 +6,7 @@ public class tester {
 		// TODO Auto-generated method stub
 
 		CommandInterpreter cli = new CommandInterpreter();
-		cli.evaluate("define munition bomb b1; // comment \ndelete window win;");
+		cli.evaluate("define munition bomb b1; // comment \ndelete window win; // comment again \n");
 		cli.evaluate("delete window win");
 		cli.evaluate("define munition missile id1 with sensor id2 fuze id3 arming distance 20.0");
 		cli.evaluate("set id1 deploy munition id2 at azimuth 20 elevation 20");
