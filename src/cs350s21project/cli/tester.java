60aacfb67a4a0cb5cd06b0512317fceaf13bdff4
Brian Jackson <brianjackson1224@gmail.com>
2021-06-04 12:45:33 -0700

    Tested all sets and munitions

--- src/cs350s21project/cli/tester.java
+++ src/cs350s21project/cli/tester.java
@@ -6,10 +6,20 @@ public class tester {
 		// TODO Auto-generated method stub
 
 		CommandInterpreter cli = new CommandInterpreter();
-		cli.evaluate("define munition bomb b1; // comment \ndelete window win; // comment again \n");
-		cli.evaluate("delete window win");
-		cli.evaluate("define munition missile id1 with sensor id2 fuze id3 arming distance 20.0");
-		cli.evaluate("set id1 deploy munition id2 at azimuth 20 elevation 20");
-		cli.evaluate("@wait 20");
+		cli.evaluate("set id course 090");
+		cli.evaluate("set id speed 25");
+		cli.evaluate("set id altitude 1500");
+		cli.evaluate("set id depth 1500");
+		cli.evaluate("define munition bomb b1;");
+		cli.evaluate("define munition shell id");
+		cli.evaluate("define munition depth_charge id1 with fuze id2");
+		cli.evaluate("define munition torpedo id1 with sensor id2 fuze id3 arming time 10.8");
+		cli.evaluate("define munition missile id1 with sensor id2 fuze id3 arming distance 25.3");
+		cli.evaluate("set id1 load munition id2");
+		cli.evaluate("set id1 deploy munition id2");
+		cli.evaluate("set id1 deploy munition id2 at azimuth 10 elevation 25");
+		
+		cli.evaluate("set id course 090;set id speed 25;set id altitude 1500; // hello\nset id depth 1500;define munition bomb b1;define munition shell id;"
+				+ ";define munition missile id1 with sensor id2 fuze id3 arming distance 25.3;set id1 deploy munition id2;//yoyoyoy\n");
 	}
 }
